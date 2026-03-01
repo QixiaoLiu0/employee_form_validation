ddb8554c57ba455085591cad3f2a0bc0cf8f83fe
Qixiao Liu <christal.liu008@gmail.com>
2026-03-01 06:41:03 -0700

    made project skeleton

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,33 +1,28 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Tabs } from "expo-router";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
+      screenOptions={{ headerShown: true, tabBarActiveTintColor: "#386de5" }}
+    >
       <Tabs.Screen
-        name="index"
+        name="signIn"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: "Sign In",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="sign-in" size={24} color={color} />
+          ),
         }}
       />
+
       <Tabs.Screen
-        name="explore"
+        name="signUp"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "Sign Up",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="registered" size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
---
+++ app/(tabs)/signIn.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function SignIn() {
+  return (
+    <View>
+      <Text>Sign In page</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
